de7a3af6b5e83e5bb2e71e3d51687e8fd89da545
Batuhan Çelik <alparslanbatuhancelik@gmail.com>
2025-10-06 21:19:15 +0300

    Initial Commit

---
+++ portfoy360-v-2/portfoy360_v_2App.swift
@@ -0,0 +1,17 @@
+//
+//  portfoy360_v_2App.swift
+//  portfoy360-v-2
+//
+//  Created by Batuhan Çelik on 6.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct portfoy360_v_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
